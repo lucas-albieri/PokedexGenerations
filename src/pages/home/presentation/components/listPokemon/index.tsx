88bbf59bcf5435dc08ed4bfadce9b7bc4fbd73d5
lucas-albieri <lucas.albisantos55@gmail.com>
2022-10-07 16:54:50 -0300

    aSD

--- src/pages/home/presentation/components/listPokemon/index.tsx
+++ src/pages/home/presentation/components/listPokemon/index.tsx
@@ -1,58 +1,103 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { Box, Button, CircularProgress, Skeleton } from "@mui/material";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { PokemonModel } from "../../../../../models/pokemon";
 import { GetAllPokemons } from "../../../../../queries/default_query";
 import Pokemon from "../pokemon";
 import SearchHome from "../search";
-import { BoxContent, Container, Content, ListPokemons } from "./styles";
+import { BoxContent, Container, Content, ListPokemons, ListPokemonsProps } from "./styles";
 
 const ListPokemon = () => {
 
     const [limit, setLimit] = useState(20);
     const [offset, setOffset] = useState(0);
-    const allPokemons = useQuery(GetAllPokemons,
+    const allPokemonsOfLimit = useQuery(GetAllPokemons,
         {
             variables: {
                 limit: limit,
                 offset: offset
             }
         });
-    // console.log(allPokemons.data.pokemon_v2_pokemon);
+    const [getPK, { data }] = useLazyQuery(GetAllPokemons,
+        {
+            variables: {
+                limit: 900,
+                offset: offset,
+                _name: "",
+                id: 0
+            }
+        });
 
+    console.log("TESTE")
+    // const pokemons = allPokemon.data?.pokemon_v2_pokemon;
+    const [pokemonFiltered, setPokemonFiltered] = useState<PokemonModel>();
+
+    const handleSearch = (_search: string) => {
+        if (_search) {
+            getPK({
+                variables: {
+                    limit: 900,
+                    offset: offset,
+                    _name: _search,
+                    id: +_search
+
+                }
+            });
+            // setPokemonFiltered(pokemons.find((pokemon: PokemonModel) =>
+            //     pokemon.name === _search || pokemon.id === parseInt(_search)));
+
+        }
+    }
 
     return (
         <Container>
             <Content>
                 <BoxContent>
-                    <SearchHome />
-                    <ListPokemons>
+
+                    <SearchHome
+                        onSearch={(search) =>
+                            handleSearch(search)
+                        }
+                    />
+
+                    <ListPokemonsProps
+                        display={allPokemonsOfLimit.loading ? "flex" : "grid"}
+                    >
                         {
-                            allPokemons.loading ? <>
+                            allPokemonsOfLimit.loading ? <>
                                 <Box sx={{
                                     display: 'flex',
                                     justifyContent: 'center',
                                     alignItems: 'center',
-                                    height: '100vh',
-                                    width: '100vw',
+                                    height: '100%',
+                                    width: '100%',
                                 }}>
                                     <CircularProgress size={70} />
                                 </Box>
                             </>
                                 :
-                                allPokemons.data && allPokemons.data.pokemon_v2_pokemon.map((pokemon: PokemonModel) => {
-                                    return (
-                                        <Pokemon
-                                            key={pokemon.id}
-                                            id={pokemon.id}
-                                            name={pokemon.name}
-                                            pokemon_v2_pokemonsprites={pokemon.pokemon_v2_pokemonsprites}
-                                            pokemon_v2_pokemontypes={pokemon.pokemon_v2_pokemontypes}
-                                        />
-                                    )
-                                })
+                                pokemonFiltered ?
+                                    <Pokemon
+                                        id={pokemonFiltered.id}
+                                        name={pokemonFiltered.name}
+                                        pokemon_v2_pokemonsprites={pokemonFiltered.pokemon_v2_pokemonsprites}
+                                        pokemon_v2_pokemontypes={pokemonFiltered.pokemon_v2_pokemontypes}
+                                    />
+                                    :
+                                    allPokemonsOfLimit.data &&
+                                    allPokemonsOfLimit.data.pokemon_v2_pokemon.map((pokemon: PokemonModel) => {
+                                        return (
+                                            <Pokemon
+                                                key={pokemon.id}
+                                                id={pokemon.id}
+                                                name={pokemon.name}
+                                                pokemon_v2_pokemonsprites={pokemon.pokemon_v2_pokemonsprites}
+                                                pokemon_v2_pokemontypes={pokemon.pokemon_v2_pokemontypes}
+                                            />
+                                        )
+                                    })
                         }
-                    </ListPokemons>
+                    </ListPokemonsProps>
 
                     <Button
                         variant="contained"
--- src/queries/default_query.ts
+++ src/queries/default_query.ts
@@ -1,8 +1,12 @@
 import { gql } from "@apollo/client";
 
 export const GetAllPokemons = gql`
-  query samplePokeAPIquery($limit: Int!, $offset: Int!) {
-  pokemon_v2_pokemon(limit: $limit, offset: $offset) {
+  query samplePokeAPIquery($limit: Int!, $offset: Int!, $_name: String!, $id: Int!) {
+  pokemon_v2_pokemon(
+    limit: $limit, offset: $offset, 
+    where: {pokemon_id: $id, pokemon_v2_pokemon: {name: $name}}
+  )
+   {
     name
     id
     pokemon_v2_pokemonsprites {
